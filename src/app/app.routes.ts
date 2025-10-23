4729d8734b11b3a8e164f63eaf6f10eb24b16552
Braian Manrique <braianm@iadb.org>
2025-10-23 09:26:27 -0500

    add bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,31 +1,9 @@
 import { Routes } from '@angular/router';
-<<<<<<< Updated upstream
 import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-=======
-import { ContadorComponent } from './pages/contador/contador.component';
-import { HeroComponent } from './pages/hero/hero.component';
-import { DragonballComponent } from './pages/dragonball/dragonball.component';
 
-export const routes: Routes = [
-  {
-    path: '',
-    component: ContadorComponent
-  },
-  {
-    path: 'hero',
-    component: HeroComponent
-  },
-    {
-    path: 'dragonball',
-    component: DragonballComponent
-  },
->>>>>>> Stashed changes
-  {
-    path : '',
-    component: AppComponent
-  },{
+ {
     path : '**',
     redirectTo : ''
   }
