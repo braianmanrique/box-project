cb86935127b8db30fcdaecc3f2995dc276710bc5
Braian Manrique <braianm@iadb.org>
2026-01-22 10:56:19 -0500

    add enviroments config

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  companyName: 'GYM MANAGER',
+  companyName2: '',
+  companySlogan: 'Gestiona tu gimnasio',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  companyName: 'BOX PAGE',
+  companyName2: 'Box',
+  companySlogan: 'Your all-in-one management solution',
+};
