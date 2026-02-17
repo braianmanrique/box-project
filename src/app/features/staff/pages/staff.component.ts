42c36bbb2fd9650339fca0fb6e7028c6c64405f8
Braian Manrique <braianm@iadb.org>
2026-02-17 13:12:42 -0500

    create staff component

---
+++ src/app/features/staff/pages/staff.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-staff',
+  standalone: true,
+  imports: [],
+  templateUrl: './staff.component.html',
+  styleUrls: ['./staff.component.css']
+})
+export class StaffComponent  {
+
+  constructor() { }
+
+
+}
